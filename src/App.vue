a9d9251f77727ef059c90a311646e6ca85fe8bd6
AdmiralXy <gameproblem80@gmail.com>
2021-09-02 21:01:26 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-col md="12" class="text-center">
+          <img alt="Логотип" src="./assets/logo.png" class="img-fluid">
+          <Header />
+          <b-form-textarea
+              id="textarea"
+              v-model="text"
+              placeholder="Enter something..."
+              rows="3"
+              max-rows="6"
+          ></b-form-textarea>
+
+          <pre class="mt-3 mb-0">{{ text }}</pre>
+          <b-button variant="primary">Создать</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap');
+  @import "./assets/scss/app.scss";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1>Feed 21</h1>
+    <p>
+      Генератор отзывов для проверяющих.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
